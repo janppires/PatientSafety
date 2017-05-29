b5cd3ad905093a483e31d9c68d0c196bcee98064
Jorge Andre Pires <jorgeandrepires@jorges-mbp.home>
2017-05-29 23:01:26 +0100

    refactor: make topics data a json file

--- src/api/topics.js
+++ src/api/topics.js
@@ -1,17 +1,8 @@
 import { normalize, schema } from 'normalizr';
+import topics from './topics.json';
 
 const topic = new schema.Entity('topics');
-const topicsSchema = [ topic ]
-
-const topics = [
-  { id: 1, name: 'Fluids', articles: [{name: 'Fluid And Electrolytes'}, {name: 'Hypercalcaemia'}, {name: 'Hyperkaleamia'}, {name: 'Hypoglycaemia'}] },
-  { id: 2, name: 'Drugs', articles: [{name: 'Common Drug Doses'}, {name: 'Drug Safety'}] },
-  { id: 3, name: 'Presenting', articles: [] },
-  { id: 4, name: 'Investigations', articles: [] },
-  { id: 5, name: 'Referrals', articles: [] },
-  { id: 6, name: 'Guidelines', articles: [] },
-  { id: 7, name: 'Procedures', articles: [] }
-]
+const topicsSchema = [ topic ];
 
 export function getTopics() {
   return new Promise((resolve, reject) => {
